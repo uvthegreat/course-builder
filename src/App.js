f0fb9917d1dab6ee2bc5e3c48170fe34a9dcb541
Utkarsh Vishwakarma <utkarshvishwakarma1411@gmail.com>
2024-05-19 12:47:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import { CustomKanban } from "./components/CustomKanban";
 function App() {
   return (
     <>
